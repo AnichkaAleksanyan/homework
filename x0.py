21f70d70c9b08dccc7efbe74b820d618a7a9ffd7
AnichkaAleksanyan <77446415+AnichkaAleksanyan@users.noreply.github.com>
2021-01-25 16:52:20 +0400

    TicTacToe changed

--- x0.py
+++ x0.py
@@ -1,7 +1,8 @@
 import random 
 all_steps = [1,2,3,4,5,6,7,8,9]
 steps = []
-win = True
+countPc = 0
+countUser = 0
 
 print('       Hi you play Tic Tac Toe ')
 print('')
@@ -28,14 +29,19 @@ def print_tablo():
 
 
 def win():
-
+	# global countUser
+	# countUser = 0
 	if all_steps[:3]==["x","x","x"] or all_steps[4:7]==["x","x","x"] or all_steps[7:10]==["x","x","x"]  or all_steps[0:9:4]==["x","x","x"] or all_steps[2:7:2]==["x","x","x"] or all_steps[0:7:3]==["x","x","x"] or all_steps[1:8:3] == ["x","x","x"] or all_steps[2:9:3]==["x","x","x"]:
-		print("win user")
+		# print("win user")
+		# countUser+=1
+		# print(countUser)
 		return False
 		
 
 	elif all_steps[:3]==["0","0","0"] or all_steps[4:7]==["0","0","0"] or all_steps[7:10]==["0","0","0"] or all_steps[0:9:4]==["0","0","0"] or all_steps[2:7:2]==["0","0","0"] or all_steps[0:7:3]==["0","0","0"] or all_steps[1:8:3] == ["0","0","0"] or all_steps[2:9:3]==["0","0","0"]:
-		print("win pc")
+		# print("win pc")
+		# countPc+=1
+		# print(countPc)
 		return False
 		
 	return True	
@@ -71,9 +77,25 @@ def ssss(startPlayer):
 		if len(steps)==9 or not win():
 			break
 
-
-firstplayer = random.choice(["pc","user"])
-ssss(print(start(firstplayer)))
+while countUser<3 and countPc<3:
+	all_steps = [1,2,3,4,5,6,7,8,9]
+	firstplayer = random.choice(["pc","user"])
+	ssss(print(start(firstplayer)))
+	if all_steps[:3]==["x","x","x"] or all_steps[4:7]==["x","x","x"] or all_steps[7:10]==["x","x","x"]  or all_steps[0:9:4]==["x","x","x"] or all_steps[2:7:2]==["x","x","x"] or all_steps[0:7:3]==["x","x","x"] or all_steps[1:8:3] == ["x","x","x"] or all_steps[2:9:3]==["x","x","x"]:
+		print("win user")
+		countUser+=1
+		print("user",countUser)
+		print("pc",countPc)
+	elif all_steps[:3]==["0","0","0"] or all_steps[4:7]==["0","0","0"] or all_steps[7:10]==["0","0","0"] or all_steps[0:9:4]==["0","0","0"] or all_steps[2:7:2]==["0","0","0"] or all_steps[0:7:3]==["0","0","0"] or all_steps[1:8:3] == ["0","0","0"] or all_steps[2:9:3]==["0","0","0"]:
+		print("win pc")
+		countPc+=1 	
+		print("user",countUser)
+		print("pc",countPc)
+else:
+	if countPc==3:
+		print("***********win Pc*************")
+	elif countUser==3:
+		print("************win User***********")		
 
 
 
